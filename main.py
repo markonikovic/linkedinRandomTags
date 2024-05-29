5fb2f195233d6aa472e1d0b1a2b842dcfcbad049
marko <markonikovic@protonmail.com>
2024-05-29 08:51:15 +0000

    wrote the simple script

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+tags = ["#Amsterdam", "#Carservice", "#limo", "#transfer",
+        "#airporttransfer", "#concierge", "#conciergeservice",
+        "#management", "#corporatetravel", "#travel", "#luxury",
+        "#groundtransportation", "#transportation", "#chauffeurdriven",
+        "#chauffeurservice", "#chauffeurs", "#chauffeur",
+        "#businesstravel", "#mercedesbenz", "#transportationindustry",
+        "#socialstrategy", "#consolidation", "#airtravel", "#limousine",
+        "#mercedes", "#NLA", "#NELA", "#GBTA", "#GCLA",  "#DMC", "#MICE",
+        "#incentivetravel", "#events", "#meetingprofs", "#limoservice",
+        "#meetingsandevents", "#airports", "#aviation",
+        "#meetingplanners", "#grouptravel", "#meetingsmeanbusiness",
+        "#groups", "#meetings", "#eventplanning", "#eventprofs",
+        "#eventplanners", "#privatejetcharter", "#privatejet",
+        "#travelagency", "#travelmanagement", "#executiveassistant",
+        "#travelindustry", "#personalassistant", "#futureofmobility",
+        "#technology", "#logistics", "#travelmarketing",
+        "#businessaviation", "#travelindustry", "#hospitality",
+        "#leadership", "#marketing", "#business", "#luxurytravel",
+        "#hotel", "#hospitalityindustry",]
+
+print(" ".join(random.sample(tags, 40)))
